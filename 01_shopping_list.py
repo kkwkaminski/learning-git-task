140da19cb5768c9d010ca2ba52f20da46fc503c7
Kamil <kkw.kaminski@gmail.com>
2023-06-18 16:04:09 +0200

    Add program for shopping list

--- 01_shopping_list.py
+++ 01_shopping_list.py
@@ -1,15 +1,3 @@
 # Kamil Kamiński
 print("\nZad 1")
-shopping_list = {
-    'piekarnia': ['chleb', 'pączek', 'bułki'],
-    'warzywniak': ['marchew', 'seler', 'rukola']
-}
-items_number = 0
 
-for shop, products in shopping_list.items():
-    shop = shop.capitalize()
-    products = [product.capitalize() for product in products]
-    items_number += len(products)
-    print(f"Idę do {shop} i kupuję tam {products}.")
-
-print(f"W sumie kupuję {items_number} produktów.")
