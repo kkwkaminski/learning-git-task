bb6abf76e24853aad3f6d6393e6484b1dc1e12a7
Kamil <kkw.kaminski@gmail.com>
2023-07-24 22:23:17 +0200

    update

--- 01_shopping_list.py
+++ 01_shopping_list.py
@@ -13,6 +13,4 @@ for shop, products in shopping_list.items():
     print(f"Idę do {shop} i kupuję tam {products}.")
 print(f"W sumie kupuję {items_number} produktów.")
 
-print("Dodany tekst w repo")
-
-print("Zmiana na test_branch")
+print("Dodany tekst w repo ")
